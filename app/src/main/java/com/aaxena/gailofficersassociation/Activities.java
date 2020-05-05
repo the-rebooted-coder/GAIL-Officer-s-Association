b693983a2d96d318c3cb0ca8a76ce8028e02bcd2
Spandan Saxena <spandansaxena@gmail.com>
2020-05-05 16:11:50 +0530

    Added New News Channel
    Added Back Button Support Functionality

--- app/src/main/java/com/aaxena/gailofficersassociation/Activities.java
+++ app/src/main/java/com/aaxena/gailofficersassociation/Activities.java
@@ -1,9 +1,9 @@
 package com.aaxena.gailofficersassociation;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.view.KeyEvent;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -11,6 +11,7 @@ import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activities extends AppCompatActivity {
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +22,31 @@ public class Activities extends AppCompatActivity {
         Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         v.vibrate(27);
 
-        final WebView webview = findViewById(R.id.news_loader);
+        webview = findViewById(R.id.news_loader);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.getSettings().setDomStorageEnabled(true);
         webview.setWebViewClient(new WebViewClient() {
         });
-        webview.loadUrl("https://www.bbc.com/news/world/asia/india");
+        webview.loadUrl("https://newsforgail.blogspot.com/");
         webview.measure(100,100);
         webview.getSettings().setUseWideViewPort(true);
         webview.getSettings().setLoadWithOverviewMode(true);
         webview.setVerticalScrollBarEnabled(false);
         webview.setHorizontalScrollBarEnabled(false);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webview.canGoBack()) {
+                    webview.goBack();
+                } else {
+                    finish();
+                }
+                return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
