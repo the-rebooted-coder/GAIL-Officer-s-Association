9e4bfdbebcd0f91ed0beecd80130ed0e5fbcd9f6
Spandan Saxena <spandansaxena@gmail.com>
2020-09-22 23:15:56 +0530

    Updated Gradle with newer implementations, Bumped Version to 20
    Prodiction Level Commits Ahead

---
+++ app/src/main/java/com/aaxena/gailofficersassociation/GailVerification.java
@@ -0,0 +1,14 @@
+package com.aaxena.gailofficersassociation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GailVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gail_verification);
+    }
+}
