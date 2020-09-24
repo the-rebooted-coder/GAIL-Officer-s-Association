c44cac331dcdcb1b3fad7f3860f0fda7122a3a50
Spandan Saxena <spandansaxena@gmail.com>
2020-09-24 22:10:19 +0530

    FirstTimeBootActivity Redesign Completed

--- app/src/main/java/com/aaxena/gailofficersassociation/Details.java
+++ app/src/main/java/com/aaxena/gailofficersassociation/Details.java
@@ -2,7 +2,6 @@ package com.aaxena.gailofficersassociation;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
@@ -10,17 +9,14 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Details extends AppCompatActivity {
-private Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_details);
-
-        start = findViewById(R.id.start);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        Button start = findViewById(R.id.start);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,12 +30,5 @@ private Button start;
 
         Vibrator v6 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         v6.vibrate(21);
-
-        //Android Background Animation
-        ConstraintLayout container = findViewById(R.id.beginning);
-        AnimationDrawable anim = (AnimationDrawable) container.getBackground();
-        anim.setEnterFadeDuration(6000);
-        anim.setExitFadeDuration(2000);
-        anim.start();
     }
 }
