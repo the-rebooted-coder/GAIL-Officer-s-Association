873ebc33b11a0dabccb99542ac6c81439f01aa75
Spandan Saxena <spandansaxena@gmail.com>
2020-09-25 18:53:33 +0530

    Finalised, Redesigning Dash

--- app/src/main/java/com/aaxena/gailofficersassociation/ChangeName.java
+++ app/src/main/java/com/aaxena/gailofficersassociation/ChangeName.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
